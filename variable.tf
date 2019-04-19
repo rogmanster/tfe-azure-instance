a6a9c90082826e3c38294315631f6351bfca2ebf
Roger Chao <rogman@gmail.com>
2019-04-18 22:00:47 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,6 @@
-variable "vm_size" { default = "Standard_DS2_v2" }
-variable "dns_prefix"  { default = "rc-instance"}
-variable "environment" { default = "rc-azure"}
+variable "vm_size"        { default = "Standard_DS2_v2" }
+variable "dns_prefix"     { default = "rc-instance"}
+variable "environment"    { default = "rc-azure"}
+variable "admin_password" { default = "pTFE1234!"}
+variable "location"       { default = "East US"}
+
